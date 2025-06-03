ccfa57437eebfcc3a436bbce10832b56881a329f
Engjell <152417866+engjellsadiku98@users.noreply.github.com>
2025-06-03 23:18:35 +0200

    push

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 function setCookie(name, value, days) {
-    const d = new Date();
-    d.setTime(d.getTime() + (days*24*60*60*1000));
-    const expires = "expires=" + d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-  }
+  const expires = new Date(Date.now() + days * 864e5).toUTCString();
+  document.cookie = `${name}=${value}; expires=${expires}; path=/`;
+}
 
-  function getCookie(name) {
-    const cname = name + "=";
-    const ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i].trim();
-      if (c.indexOf(cname) == 0) return c.substring(cname.length, c.length);
-    }
-    return "";
-  }
+function getCookie(name) {
+  return document.cookie.split('; ').reduce((r, v) => {
+    const parts = v.split('=');
+    return parts[0] === name ? decodeURIComponent(parts[1]) : r
+  }, '');
+}
 
-  function acceptCookies() {
-    setCookie("cookiesAccepted", "yes", 365);
-    document.getElementById("cookie-banner").style.display = "none";
-  }
+function acceptAllCookies() {
+  setCookie("cookiesAccepted", "all", 365);
+  setCookie("analytics", "true", 365);
+  setCookie("marketing", "true", 365);
+  hideBanner();
+}
 
-  window.onload = function() {
-    if (!getCookie("cookiesAccepted")) {
-      document.getElementById("cookie-banner").style.display = "block";
-    }
+function saveCookieSettings() {
+  const analytics = document.getElementById("analyticsCookies").checked;
+  const marketing = document.getElementById("marketingCookies").checked;
+  setCookie("cookiesAccepted", "custom", 365);
+  setCookie("analytics", analytics, 365);
+  setCookie("marketing", marketing, 365);
+  hideBanner();
+}
+
+function openSettings() {
+  document.getElementById("cookie-settings").classList.remove("hidden");
+}
+
+function hideBanner() {
+  document.getElementById("cookie-consent").classList.add("hidden");
+}
+
+window.onload = function () {
+  if (!getCookie("cookiesAccepted")) {
+    document.getElementById("cookie-consent").classList.remove("hidden");
   }
+}
